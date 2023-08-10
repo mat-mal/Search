2195420bf4a95e3b9262dd115ddb97c7ad3c9c8d
mat-mal <mateusz2005_1990@tlen.pl>
2023-08-10 17:49:45 +0200

    final

--- Search.c
+++ Search.c
@@ -1,9 +1,3 @@
-<<<<<<< Updated upstream
-#include <stdio.h>
-#include <dirent.h>
-
-int CountFiles(DIR *directory);
-=======
 #include "Search.h"
 
 
@@ -13,37 +7,6 @@ static int found = 0;
 
 int main(int argc, char *argv[])
 {
-<<<<<<< Updated upstream
-    DIR *directory = NULL;
-    struct dirent *entry;
-    directory = opendir(".");
-
-    int counter = CountFiles(directory);
-    char buffor[counter][256];
-    int i = 0;
-    while((entry = readdir(directory)) != NULL)
-    {
-        strncpy(buffor[i], entry->d_name, 256);
-        printf("%s\n", buffor[i]);
-        i++;
-    }
-    
-    //printf("%s\n", buffor[1]);
-    char search[256];
-    scanf(" %s", search);
-    printf("Searching: %s\n", search);
-
-    for(int i = 0; i < counter; i++)
-    {
-        if(strstr(buffor[i], search) != NULL)
-        {
-            printf("Result: %s\n", buffor[i]);
-        }
-    }
-
-    
-    closedir(directory);
-=======
     char opt;
 
     string path = "/";
@@ -75,20 +38,6 @@ int main(int argc, char *argv[])
 
     return 0;
 }
-
-<<<<<<< Updated upstream
-int CountFiles(DIR *directory)
-{
-    int counter = 0;
-    struct dirent *entry;
-    while((entry = readdir(directory)) != NULL)
-    {
-        counter++;
-    }
-    printf("%d\n", counter);
-    rewinddir(directory);
-    return counter;
-=======
 
 void Search(string path, string search, string* results)
 {
